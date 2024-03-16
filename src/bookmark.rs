54fb98875e4879d787dee5dc509cd82fda9532ba
Dave Rodger <braverodger@gmail.com>
2024-03-15 17:08:31 -0700

    search list

--- src/bookmark.rs
+++ src/bookmark.rs
@@ -1,9 +1,25 @@
 use serde::{Deserialize, Serialize};
 use nanoid::nanoid;
+use ratatui::text::Text;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Bookmark {
     pub title: String,
     pub command: String,
-    id: nanoid!()
+    id: String
+}
+
+impl Bookmark {
+    pub(crate) fn new(title: String, command: String) -> Self {
+        Bookmark {
+            title,
+            command,
+            id: nanoid!()
+        }
+    }
+
+    pub fn tui_text(&self) -> Text {
+        Text::from(format!("Title: {}\nCommand: {}", self.title, self.command))
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::io::prelude::*;
+use std::io::{stdout, prelude::*, BufReader};
 use std::fs::File;
-use crossterm::{event::{self, KeyEventKind}, ExecutableCommand, QueueableCommand, terminal::{
+use crossterm::{event::{self, KeyEventKind}, ExecutableCommand, terminal::{
     disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
     LeaveAlternateScreen,
 }};
 use ratatui::{
     prelude::*,
 };
+use ratatui::widgets::{Block, Borders, List, ListDirection, ListState};
 use tui_textarea::TextArea;
-use std::io::stdout;
 use bookmark::Bookmark;
 
 mod bookmark;
@@ -19,21 +19,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
 
-    let bmk = Bookmark {
-        title: "Test command".to_string(),
-        command: "grep | deez nuts".to_string(),
-    };
-    let yaml = serde_yaml::to_string(&bmk)?;
-    let mut file = File::create("bookmarks.yaml")?;
-    file.write_all(yaml.as_bytes())?;
+    let bookmark_file = File::open("bookmarks.yaml");
+    if let Err(_) = bookmark_file {
+        File::create("bookmarks.yaml")?;
+    }
+    let deserialized_bookmarks: Vec<Bookmark> = serde_yaml::from_reader(File::open("bookmarks.yaml")?)?;
+    let mut state = ListState::default();
+    state.select(Some(0)); // TODO manipulate this inside event loop
+    let command_list = List::new(
+        deserialized_bookmarks.iter()
+            .map(|b| b.tui_text())
+            .collect::<Vec<Text>>())
+        .block(Block::default().title("Saved Commands").borders(Borders::ALL))
+        .style(Style::default().fg(Color::White))
+        .highlight_style(Style::default().bg(Color::DarkGray))
+        .highlight_symbol(">>")
+        .direction(ListDirection::TopToBottom);
+
 
     let mut text_area = TextArea::default();
     loop {
         terminal.draw(|frame| {
-            let search_area = frame.size();
+            let layout = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(vec![
+                    Constraint::Length(1),
+                    Constraint::Min(2),
+                ])
+                .split(frame.size());
+
             frame.render_widget(
-                text_area.widget(), search_area
-            )
+                text_area.widget(),
+                layout[0],
+            );
+
+            frame.render_stateful_widget(
+                &command_list,
+                layout[1],
+                &mut state)
         })?;
 
         if event::poll(std::time::Duration::from_millis(16))? {
@@ -45,7 +68,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             continue;
                         }
                         event::KeyCode::Enter => {
-                            print!("{}", text_area.yank_text());
+                            print!("{}", text_area.yank_text()); // TODO does this work?
                             break;
                         }
                         _ => todo!()
@@ -55,12 +78,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    // TODO output command upon exit
-
     stdout().execute(LeaveAlternateScreen)?;
     disable_raw_mode()?;
 
     Ok(())
 }
-
-// TODO use tui_widget_list for search list maybe
