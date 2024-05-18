bd39f5fe7f43686fd602d1df02299cea187036be
Dave Rodger <braverodger@gmail.com>
2024-05-17 21:08:43 -0700

    todo was todone

--- src/config/main.rs
+++ src/config/main.rs
@@ -3,12 +3,13 @@ use std::path::PathBuf;
 use oh_hai::config::Config;
 
 /// Used by installation script to establish bookmark data file path prior to initial program run
-fn main() {
+fn main() -> Result<(), confy::ConfyError> {
     let path = std::env::args().skip(1).next();
     if let Some(p) = path {
         let config = Config {
             data_path: PathBuf::from(p)
         };
-        let _ = confy::store("oh-hai", None, config);
+        confy::store("oh-hai", None, config)?;
     }
+    Ok(())
 }
