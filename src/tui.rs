958e99e3686ae09d3a36e9cf0b6b4ee0cf8192aa
balthasar <rodgerdave@proton.me>
2024-05-31 19:26:11 -0700

    comment

--- src/tui.rs
+++ src/tui.rs
@@ -4,10 +4,10 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use ratatui::backend::CrosstermBackend;
 use ratatui::{Terminal, TerminalOptions, Viewport};
 
+// Initializes inline terminal based on height param
 pub fn init_terminal(
     inline_height: u16,
 ) -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn std::error::Error>> {
-    // stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let options = TerminalOptions {
         viewport: Viewport::Inline(inline_height),
@@ -17,7 +17,6 @@ pub fn init_terminal(
 }
 
 pub fn restore_terminal() -> Result<(), Box<dyn std::error::Error>> {
-    // stdout().execute(LeaveAlternateScreen)?;
     disable_raw_mode()?;
     Ok(())
 }
