af242882371e630610251a44bdfd6914028ef64a
balthasar <rodgerdave@proton.me>
2024-05-31 17:23:48 -0700

    comments and tidy score summary logic

--- src/matcher.rs
+++ src/matcher.rs
@@ -5,14 +5,17 @@ use crate::bookmark::Bookmark;
 
 struct CommandScore(i64, Bookmark);
 
+// Consumes and returns a sorted Vec<Bookmark> from FuzzyMatcher scores based on the input pattern
 pub fn sort(commands: Vec<Bookmark>, pattern: &str) -> Vec<Bookmark> {
-    let mut score_map: Vec<CommandScore> = Vec::new();
+    let mut bookmark_scores: Vec<CommandScore> = Vec::new(); // Holds fuzzy scores and their associated Bookmark
     let matcher = SkimMatcherV2::default();
     for mut b in commands {
         let mut title_score = None;
         let mut title_index = vec![];
         let mut command_score = None;
         let mut command_index = vec![];
+
+        // Calculate scores and update local vars
         if let Some(tuple) = matcher.fuzzy_indices(&b.title, pattern) {
             title_score = Some(tuple.0);
             title_index = tuple.1;
@@ -21,22 +24,23 @@ pub fn sort(commands: Vec<Bookmark>, pattern: &str) -> Vec<Bookmark> {
             command_score = Some(tuple.0);
             command_index = tuple.1;
         }
-        match (title_score, command_score) {
-            (Some(score), None) | (None, Some(score)) => {
-                b.title_highlights = title_index;
-                b.command_highlights = command_index;
-                score_map.push(CommandScore(score, b));
-            }
-            (Some(score1), Some(score2)) => {
-                b.title_highlights = title_index;
-                b.command_highlights = command_index;
-                score_map.push(CommandScore(score1 + score2, b))
-            }
-            _ => (),
-        }
+
+        // Update bookmark highlights
+        b.title_highlights = title_index;
+        b.command_highlights = command_index;
+
+        // Sum scores and add to list
+        let combined_score = title_score.unwrap_or_default() + command_score.unwrap_or_default();
+        bookmark_scores.push(CommandScore(combined_score, b));
     }
-    score_map.sort_by(|a, b| a.0.cmp(&b.0).reverse());
-    score_map.into_iter().map(|x| x.1).collect()
+
+    // Eliminate zero scores, sort by best match, and return only the Bookmarks
+    bookmark_scores = bookmark_scores
+        .into_iter()
+        .filter(|b| b.0.is_positive())
+        .collect();
+    bookmark_scores.sort_by(|a, b| a.0.cmp(&b.0).reverse());
+    bookmark_scores.into_iter().map(|x| x.1).collect()
 }
 
 #[cfg(test)]
@@ -45,9 +49,9 @@ mod tests {
 
     #[test]
     fn score_reorder() {
-        let a = Bookmark::new("banana".to_string(), "i am a banana".to_string());
-        let b = Bookmark::new("apple".to_string(), "not a banana".to_string());
-        let c = Bookmark::new("gun".to_string(), "bang".to_string());
+        let a = Bookmark::new("banana", "i am a banana");
+        let b = Bookmark::new("apple", "not a banana");
+        let c = Bookmark::new("gun", "bang");
         let x = vec![a, b, c];
         println!("{:#?}", &x);
         let y = sort(x.clone(), "app");
