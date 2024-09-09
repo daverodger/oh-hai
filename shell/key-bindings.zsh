048a5c3bfeb7f6f19c819fbc91f57758d8c5936b
balthasar <rodgerdave@proton.me>
2024-09-08 19:47:30 -0700

    stdout passthrough with file desc

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -4,25 +4,21 @@ function __oh_hai_save__() {
   oh-hai -i "$command"
   BUFFER=""
   CURSOR=0x7fffffff
-  zle reset-promptf
+  zle reset-prompt
 }
 
 function __oh_hai_search__() {
   local query res
   query="${BUFFER}"
 
-  # Create a file descriptor to point to stdout
+  # Use file descriptor to allow stdout passthrough while capturing stderr
   exec 3>&1
-
-  # Capture stderr in the variable 'res' while allowing stdout to pass through
   res=$(oh-hai -s "$query" 2>&1 1>&3)
-
-  # Close the file descriptor
   exec 3>&-
 
   BUFFER="${res}"
   CURSOR=0x7fffffff
-  zle reset-promptf
+  zle reset-prompt
 }
 
 zle -N __oh_hai_save__
